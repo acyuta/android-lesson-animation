5b98b5d6782ed262a8d2f3c6c8dafb70d53f8121
Akim Glushkov <mail@glushkova.kim>
2019-02-16 13:13:31 +0700

    добавлены опции с возможностью показа иконки на тулбаре

--- app/src/main/java/tsu/android/animated/MainActivity.java
+++ app/src/main/java/tsu/android/animated/MainActivity.java
@@ -3,12 +3,48 @@ package tsu.android.animated;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    View layoutOnScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        layoutOnScreen = findViewById(R.id.layout);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.animations, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_kotik:
+                saySomethingCat();
+                return true;
+            case R.id.menu_snake:
+                saySomethingSnake();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void saySomethingSnake() {
+        Snackbar.make(layoutOnScreen, "Пшшш-шшш-шшш", Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void saySomethingCat() {
+        Toast.makeText(this, "Мяу-мяу", Toast.LENGTH_SHORT).show();
     }
 }
